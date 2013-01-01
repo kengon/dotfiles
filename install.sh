d9e2457bc433be00e249d2300f6fa7da05400d56
kengon <sakaikengo@gmail.com>
2013-01-01 17:04:12 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DOTFILES_DIR=$HOME/dotfiles
+
+mkdir $HOME/bin $HOME/local $HOME/share $HOME/src $HOME/tmp
+ln -s $DOTFILES_DIR/screen/screenrc $HOME/.screenrc
+ln -s $DOTFILES_DIR/vim/vimrc       $HOME/.vimrc
+ln -s $DOTFILES_DIR/vim/            $HOME/.vim
+ln -s $DOTFILES_DIR/zsh/zshrc       $HOME/.zshrc
+ln -s $DOTFILES_DIR/gdb/gdbinit     $HOME/.gdbinit
+ln -s $DOTFILES_DIR/irb/irbrc       $HOME/.irbrc
